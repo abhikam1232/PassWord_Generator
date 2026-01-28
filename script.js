6943ee8a85a5717036fb22a7ebb3006d676c008d
Abhishek <abhikam152002@gmail.com>
2026-01-28 18:38:00 +0530

    project completed

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 let myInput = document.querySelector(".input");
-let len = 6;
+let btn = document.querySelector(".btn");
+
+let len = 10;
 let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowerCase = "abcdefghijklmnopqrstuvwxyz";
 let nums = "1234567890";
 let symbols = "!@#$%^&*()_+-[]{}";
+
+let Chars = upperCase + lowerCase + nums + symbols;
+
+function generatePassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += nums[Math.floor(Math.random() * nums.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while (len > password.length) {
+        password += Chars[Math.floor(Math.random() * Chars.length)];
+    }
+
+    myInput.value = password;
+
+}
+
+btn.addEventListener("click", function () {
+    generatePassword();
+})
