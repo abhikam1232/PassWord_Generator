2262ab3f90702b5b0c263ded775e2c7ab00f989a
Abhishek <abhikam152002@gmail.com>
2026-01-28 17:47:40 +0530

    JS implementing

---
+++ script.js
@@ -0,0 +1,6 @@
+let myInput = document.querySelector(".input");
+let len = 6;
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let nums = "1234567890";
+let symbols = "!@#$%^&*()_+-[]{}";
